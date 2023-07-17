154217ea47c14f20fb471fa18f0672101bfa36fd
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-07-17 09:47:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
 import { App } from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
